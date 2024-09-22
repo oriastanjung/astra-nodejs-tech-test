764ee7346f419823f67151945c3adca4973cb2c0
oriastanjung <oriastan999@gmail.com>
2024-09-22 11:49:54 +0700

    feat: file system read

--- index.js
+++ index.js
@@ -1,8 +1,60 @@
+const fs = require('fs').promises;
+const path = require('path');
 
-const main = () => {
+const getFileTree = async (dirPath) => {
+    try {
+        const resolvedPath = path.resolve(dirPath);
+        const stats = await fs.stat(resolvedPath);
 
+        if (stats.isFile()) {
+            const fileMetadata = await getFileMetadata(resolvedPath);
+            return [fileMetadata];
+        } else if (stats.isDirectory()) {
+            const files = await fs.readdir(resolvedPath);
+            const fileDetailsPromises = files.map(file => getFileMetadata(path.join(resolvedPath, file)));
+            const fileDetails = await Promise.all(fileDetailsPromises);
+            return fileDetails.sort((a, b) => a.fileName.localeCompare(b.fileName));
+        }
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('Invalid Path');
+        } else {
+            throw error;
+        }
+    }
 };
 
+const getFileMetadata = async (filePath) => {
+    const stats = await fs.stat(filePath);
+    const createdAt = stats.birthtime.toISOString().split('T')[0]; 
 
+    
+    const projectRoot = path.resolve(__dirname);
+    
+    
+    const relativePath = path.relative(projectRoot, filePath).replace(/\\/g, '/');
+    const formattedPath = `/${relativePath.replace(/^(..\/)+/, '')}`; 
+
+    return {
+        fileName: path.basename(filePath),
+        filePath: formattedPath, 
+        size: stats.size,
+        createdAt: createdAt,
+        isDirectory: stats.isDirectory(),
+    };
+};
+
+const main = async () => {
+    const testPath = path.join(__dirname, 'tmp', 'test_dir'); 
+    try {
+        const result = await getFileTree(testPath);
+        console.log(JSON.stringify(result, null, 2));
+    } catch (err) {
+        console.error(err.message);
+    }
+};
 
 module.exports = main;
+
+
+main();
